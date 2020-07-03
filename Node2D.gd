8301c127842d2011ac37142d4d62b5cbb7e69345
Sylvain <sylvain.daronnat@strath.ac.uk>
2020-07-03 20:22:14 +0100

    first working albeit cpu heavy version

--- Node2D.gd
+++ Node2D.gd
@@ -1,109 +1,96 @@
 extends Node2D
 
-var X_CUT = 2
-var Y_CUT = 3
-
+var X_CUT = 15
+var Y_CUT = 15
 var frame_counter = 0
-#var fragment_counter = 0
-#var fragments
+var all_fragments = []
+var start_process = false
+#var weight = 900
 
 func _ready():
-#	print($Sprite.get_rect())
 	$Sprite.set_vframes(Y_CUT)
 	$Sprite.set_hframes(X_CUT)
-	
-#	test_generate_rigid_body()
-	
-var debug_counter = 0
+
+#var debug_counter = 0
 func _process(delta):
 	$fps.text = "FPS: "+str(Engine.get_frames_per_second())
-	debug_counter+=delta
-	if debug_counter > 0.5:
-		debug_counter = 0
-		
-		
-		
-		if frame_counter < X_CUT*Y_CUT:
-			generate_fragment(frame_counter)
-			frame_counter+=1
-			
+#	debug_counter+=delta
+#	if debug_counter > 0.5:
+	if frame_counter < X_CUT*Y_CUT: # and start_process == true
+		generate_fragment(frame_counter)
+		frame_counter+=1
+
 func generate_fragment(i):
-	print(i)
-#	$Sprite.set_frame(i)
-	
-	var fragmt = RigidBody2D.new()
-	
+	## DIVIDING SPRITE ##
 	var fragmt_sprite = Sprite.new()
 	fragmt_sprite.set_texture($Sprite.texture)
+	fragmt_sprite.scale = $Sprite.scale
 	fragmt_sprite.set_vframes(Y_CUT)
 	fragmt_sprite.set_hframes(X_CUT)
 	fragmt_sprite.set_frame(i)
 	
+	## COLLISION SHAPE ##
 	var fragmt_shape = CollisionShape2D.new()
 	var rect_shape = RectangleShape2D.new()
+	var sprite_size = fragmt_sprite.get_rect().position*$Sprite.scale
 	rect_shape.set_extents(Vector2(16,10.5))
-	rect_shape.set_extents(fragmt_sprite.get_rect().position)
+	rect_shape.set_extents(sprite_size)
 	fragmt_shape.set_shape(rect_shape)
 	
-	fragmt.name = "GENERATED BODY"
-	fragmt.add_child(fragmt_sprite)
-	fragmt.add_child(fragmt_shape)
-	fragmt.set_position($debug_spawn.position)
-	
-	add_child(fragmt)
-	
-#	$Sprite.set_frame(i)
-	
-
-func test_generate_rigid_body():
+	## ASSEMBLING IT INTO A RIGIDBODY ##
 	var fragmt = RigidBody2D.new()
-	var fragmt_sprite = Sprite.new()
-	fragmt_sprite.set_texture($debug_sprite.texture)
-	
-	var fragmt_shape = CollisionShape2D.new()
-	var rect_shape = RectangleShape2D.new()
-	rect_shape.set_extents(Vector2(50,20))
-	fragmt_shape.set_shape(rect_shape)
-	
 	fragmt.name = "GENERATED BODY"
 	fragmt.add_child(fragmt_sprite)
 	fragmt.add_child(fragmt_shape)
-	fragmt.set_position($debug_spawn.position)
+	fragmt.set_mode(RigidBody2D.MODE_STATIC)
 	
+	# SET PIECES TO PROPER POSITIONS
+#	fragmt.set_position($debug_spawn.position)
+	var new_pos = $debug_spawn.position
+	new_pos.x -= (fragmt_sprite.get_frame_coords().x+1)*(sprite_size.x*2)
+	new_pos.y -= (fragmt_sprite.get_frame_coords().y+1)*(sprite_size.y*2)
+	fragmt.set_position(new_pos)
+	
+	all_fragments.append(fragmt)
 	add_child(fragmt)
-#	var instance_fragmt = fragmt.instance()
-#	self.get_parent().add_child(instance_fragmt)
-	
-#	get_all_children()
+#	if i+1 == Y_CUT*X_CUT:
+#		for f in all_fragments:
+#			f.set_mode(RigidBody2D.MODE_RIGID)
+#			f.set_applied_torque(1000)
+#			var new_force = Vector2(0,0)
+#			new_force.x = rand_range(-100,100)
+#			new_force.y = rand_range(-300,0)
+#			f.set_linear_velocity(new_force)
+#			$despawn_tween.interpolate_property(f,"scale",
+#			f.scale,Vector2(0,0),2.5,
+#			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+#			$despawn_tween.start()
+
+func destruction():
+	for f in all_fragments:
+		f.set_mode(RigidBody2D.MODE_RIGID)
+		f.set_applied_torque(1000)
+		var new_force = Vector2(0,0)
+		new_force.x = rand_range(-100,100)
+		new_force.y = rand_range(-200,0)
+		f.set_linear_velocity(new_force)
+		$despawn_tween.interpolate_property(f,"scale",
+		f.scale,Vector2(0,0),3.5,
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$despawn_tween.start()
+
 
 func get_all_children():
 	for c in get_children():
 		prints(c,c.name)
 
 
+func _on_despawn_tween_tween_completed(object, key):
+	object.queue_free()
 
-#	fragments = generate_fragment()
-#
-#var debug_counter = 0
-#func _process(delta):
-#	debug_counter+=delta
-#	if debug_counter > 0.5:
-#		debug_counter = 0
-#		select_frame()
-#
-#func generate_fragment():
-#	fragments = []
-#	var i = 0
-#	for y in Y_CUT:
-#		for x in X_CUT:
-#			i+=1
-#			fragments.append(Vector2(x,y))
-#	print(fragments)
-#	return fragments
-#
-#func select_frame():
-#	$Sprite.set_vframes(fragments[fragment_counter].x)
-#	$Sprite.set_hframes(fragments[fragment_counter].y)
-#	if fragment_counter < len(fragments)-1:
-#		fragment_counter += 1
 
+func _on_Button_pressed():
+	destruction()
+	$Button.disabled = true
+#	start_process = true
+	
